fdcb0ed283b1e63349ef0008c8ebd5a220001609
Tayná Tinôco <tayna-ct12@hotmail.com>
2021-10-06 11:20:10 +0100

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -2,3 +2,10 @@ var user = "JaneDoe";
 var password = "hello world!";
 console.log(password);
 console.log(user);
+
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
