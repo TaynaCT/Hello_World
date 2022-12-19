57e51d9d6a0f95bec21f261e294b6764136bc888
Tayná Tinôco <tayna-ct12@hotmail.com>
2022-12-19 16:12:41 +0000

    app folder creation

---
+++ app/app-hello-world.js
@@ -0,0 +1,31 @@
+var user = "JaneDoe";
+var password = "hello world!";
+console.log(password);
+console.log(user);
+
+
+var x = document.getElementById("demo");
+var longitude;
+var latitude;
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+	longitude = position.coords.longitude;
+	latitude = position.coords.latitude;
+	
+  x.innerHTML = "Latitude: " + latitude +
+  "<br>Longitude: " + longitude;
+}
+
+
+console.log(password + x);
+
+//Some Coment
+
